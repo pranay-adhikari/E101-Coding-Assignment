57d33d7aaf79e88a68c23db5b785d4119770d471
Pranay <pranay.adhi@gmail.com>
2025-11-12 20:47:48 -0600

    Created book class, added functionality

--- main.py
+++ main.py
@@ -1,88 +1,118 @@
 from library_books import library_books
 from datetime import datetime, timedelta
 
-# -------- Level 1 --------
-# TODO: Create a function to view all books that are currently available
-# Output should include book ID, title, and author
+# Menu will loop until user exits
+menu_loop = True
 
-# Prints the title, ID, and author of all books available for checkout
-def available_books():
-    # I had to use list comprehension to convert the generator object to a list so I could check if the list was empty or not
-    available_books = [book for book in library_books if book["available"]]
-    if not available_books:
-        print("There are currently no available books")
-    else:
-        print("Available books:")
-        for book in available_books:
-            print(f'ID: {book["id"]}, Title: {book["title"]}, Author: {book["author"]}')
+# Book class that can be used to create book objects
+class Book:
+    # Static variable library to store book objects
+    library = []
 
-# -------- Level 2 --------
-# TODO: Create a function to search books by author OR genre
-# Search should be case-insensitive
-# Return a list of matching books
+    # Create and store book objects in library
+    def __init__(self, id, title, author, genre, available, due_date, checkouts):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.available = available
+        self.due_date = due_date
+        self.checkouts = checkouts
+        Book.library.append(self)
 
-# Loops through the books returning a list of all books with author or genre matches. The search query is case-insensitive, and allows partial inputs
-def search_book():
-    matching_books = []
-    query = input("Search for a book by author or genre: ").strip().lower()
-    for book in library_books:
-        author = book["author"].lower()
-        genre = book["genre"].lower()
-        if query in author or query in genre:
-            matching_books.append(book)
-    return matching_books
+    # -------- Level 1 --------
+    # TODO: Create a function to view all books that are currently available
+    # Output should include book id, title, and author
 
-# -------- Level 3 --------
-# TODO: Create a function to checkout a book by ID
-# If the book is available:
-#   - Mark it unavailable
-#   - Set the due_date to 2 weeks from today
-#   - Increment the checkouts counter
-# If it is not available:
-#   - Print a message saying it's already checked out
+    # Prints the title, id, and author of all books available for checkout
+    def available_books():
+        available_books = [book for book in Book.library if book.available]
+        if not available_books:
+            print("There are currently no available books")
+        else:
+            print("Available books:")
+            for book in available_books:
+                print(f'ID: {book.id}, Title: {book.title}, Author: {book.author}')
 
-# Attempts to checkout status of the book with the cooresponding ID parameter, printing a failure message otherwise or in the case of invalid ID
-def checkout_book(ID):
-    # Because we're accepting user input, we have to make sure the book actually exists
-    book = check_valid_id(ID)
-    if book is None:
-        print(f"There is no book with ID: {ID}")
-        return -1
+    # -------- Level 2 --------
+    # TODO: Create a function to search books by author OR genre
+    # Search should be case-insensitive
+    # Return a list of matching books
 
-    # If the book is available, we check it out. Otherwise, we print that it's already been checked out
-    if book["available"]:
-        due_date = (datetime.today() + timedelta(days=14)).strftime("%Y-%m-%d")
-        book["due_date"] = due_date
-        book["available"] = False
-        book["checkouts"] += 1
-        print(f"You've checked out \"{book["title"]}\". Its due date is {book["due_date"]}.")
-    else:
-        print(f"\"{book["title"]}\" is already checked out")
+    # Loops through the books returning a list of all books with author or genre matches. The search query is case-insensitive, and allows partial inputs
+    def search_for_book():
+        matching_books = []
+        query = input("Search for a book by author or genre: ").strip().lower()
+        for book in Book.library:
+            author = book.author.lower()
+            genre = book.genre.lower()
+            if query in author or query in genre:
+                matching_books.append(book)
+        return matching_books
 
-# -------- Level 4 --------
-# TODO: Create a function to return a book by ID
-# Set its availability to True and clear the due_date
-def return_book(ID):
-    book = check_valid_id(ID)
-    if book is None:
-        print(f"There is no book with ID: {ID}")
-        return -1
-    if book["available"]:
-        print(f"\"{book["title"]}\" is not checked out.")
-    else:
-        book["available"] = True
-        book["due_date"] = None
-        print(f"\"{book["title"]}\" successfully returned.")
+    # -------- Level 3 --------
+    # TODO: Create a function to checkout a book by id
+    # If the book is available:
+    #   - Mark it unavailable
+    #   - Set the due_date to 2 weeks from today
+    #   - Increment the checkouts counter
+    # If it is not available:
+    #   - Print a message saying it's already checked out
 
-# TODO: Create a function to list all overdue books
-# A book is overdue if its due_date is before today AND it is still checked out
-def list_overdue_books():
-    today = datetime.today().date()
-    for book in library_books:
-        if not book["available"]:
-            due_date = datetime.strptime(book["due_date"], "%Y-%m-%d").date()
-            if due_date < today:
-                print(book)
+    # Attempts to checkout status of the book with the cooresponding id parameter, printing a failure message otherwise or in the case of invalid id
+    def checkout_book(id):
+        # Because we're accepting user input, we have to make sure the book actually exists
+        book = Book.get_book(id)
+        if book is None:
+            print(f"There is no book with id: {id}")
+            return -1
+
+        # If the book is available, we check it out. Otherwise, we print that it's already been checked out
+        if book.available:
+            due_date = (datetime.today() + timedelta(days=14)).strftime("%Y-%m-%d")
+            book.due_date = due_date
+            book.available = False
+            book.checkouts += 1
+            print(f"You've checked out \"{book.title}\". Its due date is {book.due_date}.")
+        else:
+            print(f"\"{book.title}\" is already checked out")
+
+    # -------- Level 4 --------
+    # TODO: Create a function to return a book by id
+    # Set its availability to True and clear the due_date
+    def return_book(id):
+        book = Book.get_book(id)
+        if book is None:
+            print(f"There is no book with id: {id}")
+            return -1
+        if book.available:
+            print(f"\"{book.title}\" is not checked out.")
+        else:
+            book.available = True
+            book.due_date = None
+            print(f"\"{book.title}\" successfully returned.")
+
+    # TODO: Create a function to list all overdue books
+    # A book is overdue if its due_date is before today AND it is still checked out
+    def list_overdue_books():
+        today = datetime.today().date()
+        for book in Book.library:
+            if not book.available:
+                due_date = datetime.strptime(book.due_date, "%Y-%m-%d").date()
+                if due_date < today:
+                    print(book)
+
+    # -------- Helper Functions --------
+    # If a book with the id {id} exists, we return the book. Otherwise, false
+    @staticmethod
+    def get_book(id):
+        for book in Book.library:
+            if book.id == id:
+                return book
+        return None
+    
+    def __str__(self):
+        return f"ID: {self.id}, Title: {self.title}, Author: {self.author}, Genre: {self.genre}, Available: {self.available}, Checkouts: {self.checkouts}"
 
 # -------- Level 5 --------
 # TODO: Convert your data into a Book class with methods like checkout() and return_book()
@@ -91,22 +121,32 @@ def menu():
     choice = input("What would you like to do? Enter your choice:\n1: View all books\n2: Search for a book\n3: Checkout a book\n4: Return a book\n5: Exit\n")
     match choice:
         case "1":
-            checkout_book()
+            Book.available_books()
         case "2":
-            search_book()
+            # Because the instructions explicitly say to return the list of books and not just print them, I return them and then print them afterwards
+            search_results = Book.search_for_book()
+            for book in search_results:
+                print(book)
+            if not search_results:
+                print("No books match your search")
         case "3":
-            ID = input("ID of the book to checkout: ")
-            checkout_book(ID)
+            id = input("ID of the book to checkout: ")
+            Book.checkout_book(id)
         case "4":
-            ID = input("ID of the book to return: ")
-            return_book(ID)
+            id = input("ID of the book to return: ")
+            Book.return_book(id)
         case "5":
+            global menu_loop
+            menu_loop = False
             print("Goodbye!")
-            return -1
         
         # For handling default case
         case _:
             print("Not a valid option")
+
+def populate_library():
+    for book in library_books:
+        Book(book["id"], book["title"], book["author"], book["genre"], book["available"], book["due_date"], book["checkouts"])
 
 # -------- Optional Advanced Features --------
 # You can implement these to move into Tier 4:
@@ -116,19 +156,8 @@ def menu():
 # - Save/load catalog to file (CSV or JSON)
 # - Anything else you want to build on top of the system!
 
-# -------- Helper Functions --------
-
-# If a book with the id {ID} exists, we return the book. Otherwise, return None
-def check_valid_id(ID):
-    book_to_checkout = None
-    for book in library_books:
-        if book["id"] == ID:
-            book_to_checkout = book
-            return book_to_checkout
-    if book_to_checkout == None:
-        return None
-
-
 if __name__ == "__main__":
-    # You can use this space to test your functions
-    menu()
+    populate_library()
+    while menu_loop:
+        menu()
+        print()
