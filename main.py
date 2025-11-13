2b2be805dfbc073def79c99dac1dfac3ebd1d96e
Pranay <pranay.adhi@gmail.com>
2025-11-12 23:01:48 -0600

    Added json export functionality, adding/removing book functionality, and fixed menu loop by adding options 5-9 and option 0

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from library_books import library_books
 from datetime import datetime, timedelta
+import json
 
 # Menu will loop until user exits
 menu_loop = True
@@ -25,6 +26,7 @@ class Book:
     # Output should include book id, title, and author
 
     # Prints the title, id, and author of all books available for checkout
+    @staticmethod
     def available_books():
         available_books = [book for book in Book.library if book.available]
         if not available_books:
@@ -40,6 +42,7 @@ class Book:
     # Return a list of matching books
 
     # Loops through the books returning a list of all books with author or genre matches. The search query is case-insensitive, and allows partial inputs
+    @staticmethod
     def search_for_book():
         matching_books = []
         query = input("Search for a book by author or genre: ").strip().lower()
@@ -60,6 +63,7 @@ class Book:
     #   - Print a message saying it's already checked out
 
     # Attempts to checkout status of the book with the cooresponding id parameter, printing a failure message otherwise or in the case of invalid id
+    @staticmethod
     def checkout_book(id):
         # Because we're accepting user input, we have to make sure the book actually exists
         book = Book.get_book(id)
@@ -80,6 +84,7 @@ class Book:
     # -------- Level 4 --------
     # TODO: Create a function to return a book by id
     # Set its availability to True and clear the due_date
+    @staticmethod
     def return_book(id):
         book = Book.get_book(id)
         if book is None:
@@ -94,13 +99,41 @@ class Book:
 
     # TODO: Create a function to list all overdue books
     # A book is overdue if its due_date is before today AND it is still checked out
+    @staticmethod
     def list_overdue_books():
         today = datetime.today().date()
+        overdue_books = []
         for book in Book.library:
             if not book.available:
                 due_date = datetime.strptime(book.due_date, "%Y-%m-%d").date()
                 if due_date < today:
-                    print(book)
+                    overdue_books.append(book)
+        if not overdue_books:
+            print("No overdue books")
+        else:
+            for book in overdue_books:
+                print(f'Overdue: {book.title} by {book.author}, Due: {book.due_date}')
+        
+
+    # Prints 3 most checked out books
+    @staticmethod
+    def most_checked_out():
+        # We can use a lambda expression here instead of creating a new function to pass in for the key
+        by_checkouts = sorted(Book.library, key=lambda b: b.checkouts, reverse=True)
+        if not by_checkouts:
+            print("There are no books in the library")
+        else:
+            for book in by_checkouts[:3]:
+                print(book)
+    
+    # Exports catalog to json file
+    @staticmethod
+    def export_to_json():
+        books_data = Book.library_to_dictionaries()
+        file_name = "library_books.json"
+        with open(file_name, 'w') as json_file:
+            json.dump(books_data, json_file, indent=4)
+        
 
     # -------- Helper Functions --------
     # If a book with the id {id} exists, we return the book. Otherwise, false
@@ -111,6 +144,22 @@ class Book:
                 return book
         return None
     
+    @staticmethod
+    def library_to_dictionaries():
+        library_list = []
+        for book in Book.library:
+            book_dict = {"id":book.id, "title":book.title, "author":book.author, "available":book.available, "due_date":book.due_date, "checkouts":book.checkouts}
+            library_list.append(book_dict)
+        return library_list
+    
+    # Returns whether or not {id} is unique
+    @staticmethod
+    def unique_id(id):
+        for book in Book.library:
+            if book.id == id:
+                return False
+        return True
+
     def __str__(self):
         return f"ID: {self.id}, Title: {self.title}, Author: {self.author}, Genre: {self.genre}, Available: {self.available}, Checkouts: {self.checkouts}"
 
@@ -118,7 +167,9 @@ class Book:
 # TODO: Convert your data into a Book class with methods like checkout() and return_book()
 # TODO: Add a simple menu that allows the user to choose different options like view, search, checkout, return, etc.
 def menu():
-    choice = input("What would you like to do? Enter your choice:\n1: View all books\n2: Search for a book\n3: Checkout a book\n4: Return a book\n5: Exit\n")
+    print('-------------------------------')
+    choice = input("What would you like to do? Enter your choice:\n1: View available books\n2: Search for a book\n3: Checkout a book\n4: Return a book\n5: View most checked out books\n6: List overdue books\n7: Add a book\n8: Remove a book\n9: Export catalog to JSON\n0: Exit\n")
+    print()
     match choice:
         case "1":
             Book.available_books()
@@ -136,6 +187,16 @@ def menu():
             id = input("ID of the book to return: ")
             Book.return_book(id)
         case "5":
+            Book.most_checked_out()
+        case "6":
+            Book.list_overdue_books()
+        case "7":
+            add_book()
+        case "8":
+            delete_book()
+        case "9":
+            Book.export_to_json()
+        case "0":
             global menu_loop
             menu_loop = False
             print("Goodbye!")
@@ -148,6 +209,27 @@ def populate_library():
     for book in library_books:
         Book(book["id"], book["title"], book["author"], book["genre"], book["available"], book["due_date"], book["checkouts"])
 
+# When adding a book for the first time, it should be available and have no due date/checkouts as well as a unique id
+def add_book():
+    id = input("ID: ").strip()
+    if not Book.unique_id(id):
+        print("Not a unique id")
+        return -1
+    title = input("Title: ")
+    author = input("Author: ")
+    genre = input("Genre: ")
+    Book(id=id, title=title, author=author, genre=genre, available=True, due_date=None, checkouts=0)
+
+# Deletes a book from the catalog
+def delete_book():
+    id = input("ID: ")
+    book = Book.get_book(id)
+    if book is None:
+        print("Invalid ID")
+    else:
+        Book.library.remove(book)
+        print(f'\'{book.title}\' has been removed from the catalog')
+
 # -------- Optional Advanced Features --------
 # You can implement these to move into Tier 4:
 # - Add a new book (via input) to the catalog
@@ -157,7 +239,10 @@ def populate_library():
 # - Anything else you want to build on top of the system!
 
 if __name__ == "__main__":
-    populate_library()
-    while menu_loop:
-        menu()
-        print()
+    try:
+        populate_library()
+        while menu_loop:
+            menu()
+            print()
+    except KeyboardInterrupt:
+        print("Goodbye!")
